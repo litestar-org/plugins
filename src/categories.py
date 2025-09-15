3542185778bbf92c8cb4e4bef0ef0ca4338169df
Julien <courtes.julien@gmail.com>
2025-09-15 15:00:06 +0200

    fix: update categories

--- src/categories.py
+++ src/categories.py
@@ -1,10 +1,16 @@
 from typing import Literal
 
 Category = Literal[
+    "AI",
+    "Analytics",
+    "Authentication",
+    "Background Processing",
     "Database",
     "Deployment",
     "Monitoring",
     "Performance",
+    "Serialization",
     "Security",
     "Testing",
+    "UI",
 ]
